d4fbd27801b97c4e66e99d62958fc417f0788748
Adam Burrough <adamjamesburrough@gmail.com>
2023-06-12 11:46:43 +0900

    bugfix: stats pathway

--- financial/api.py
+++ financial/api.py
@@ -1,18 +1,25 @@
 from models import financial_data
-from financial.api_controller import get_all_financial_data, get_financial_data_page
+from financial.api_controller import get_all_financial_data, get_financial_data_page,statistics
 from flask import Blueprint
 
 
 api_bp = Blueprint('api', __name__)
 
+# This route will return a table of the whole database
 @api_bp.route("/financial_data/all")
 def all():
     response = get_all_financial_data()
     return f"{response}"
 
-
+#this route will return 
 @api_bp.route("/financial_data")
 def finance():
     response = get_financial_data_page()
     return f"{response}"
+
+
+@api_bp.route("/statistics")
+def get_statistics():
+    response = statistics()
+    return f"{response}",200
 
--- financial/api_controller.py
+++ financial/api_controller.py
@@ -42,15 +42,15 @@ def save_financial_data(data_list):
         return f"Error saving financial data: {str(e)}"
     
 def get_financial_data_page():
-
     #creates Start Date of search (default is today)
-    start_date = request.args.get('start_date', default=(datetime.now().strftime('%Y-%m-%d')), type=str)
+    start_date = request.args.get('start_date',default='1970-01-01', type=str)
     start_date_obj = datetime.strptime(start_date, "%Y-%m-%d").date()
 
     #creates End day of search (default is Unix epoch)
-    end_date = request.args.get('end_date', default='1970-01-01', type=str)
+    end_date = request.args.get('end_date',  default=(datetime.now().strftime('%Y-%m-%d')), type=str)
     end_date_obj = datetime.strptime(end_date, "%Y-%m-%d").date()
 
+    #will send error if start date is later than end date
     if start_date_obj > end_date_obj:
         error_message = 'Start date cannot be later than the end date.'
         response = {
@@ -114,3 +114,79 @@ def get_financial_data_page():
         }
 
     return response
+
+def statistics():
+     #creates Start Date of search (default is today)
+    start_date = request.args.get('start_date',default='1970-01-01' , type=str)
+    start_date_obj = datetime.strptime(start_date, "%Y-%m-%d").date()
+
+    #creates End day of search (default is Unix epoch)
+    end_date = request.args.get('end_date',default=(datetime.now().strftime('%Y-%m-%d')) , type=str)
+    end_date_obj = datetime.strptime(end_date, "%Y-%m-%d").date()
+    
+    # symbol is defined (return all results is symbol is not provided)
+    symbol = request.args.get('symbol', default=False, type=str)
+
+    if start_date_obj > end_date_obj:
+        error_message = 'Start date cannot be later than the end date.'
+        response = {
+            'data': [],
+            'info': {
+                'error': error_message
+            }
+        }
+        return response
+    try:
+        # fetchs all data with the symbol
+        all_data =  financial_data.query.filter_by(symbol=symbol)
+
+        #this dictonary will store the total value of all values between the two date
+        total_data = {
+            "total_daily_open_price": 0,
+            "total_daily_close_price": 0,
+            "total_daily_volume": 0,
+            "total_count": 0
+        }
+
+        #this loop will create a dict 
+        for data in all_data:
+        #     if start_date_obj <= data.date <= end_date_obj:
+        #         total_data["total_daily_open_price"] = total_data["total_daily_open_price"] + data.open_price
+        #         data_dict = {
+        #             'id': data.id,
+        #             'symbol': data.symbol,
+        #             'date': data.date.strftime('%Y-%m-%d'),
+        #             'open_price': data.open_price,
+        #             'close_price': data.close_price,
+        #             'volume': data.volume
+        #         }
+        #         data_list.append(data_dict)
+        # return total_data
+        
+            # print(data)
+            if start_date_obj <= data.date <= end_date_obj:
+                total_data["total_daily_open_price"] = total_data["total_daily_open_price"] + data.open_price
+                total_data["total_daily_close_price"] = total_data["total_daily_close_price"] + data.close_price
+                total_data["total_daily_volume"]= total_data["total_daily_volume"] + data.volume
+                total_data["total_count"] = total_data["total_count"] + 1
+                    
+
+        averaged_obj = {
+            "start_date": start_date,
+            "end_date":end_date,
+            "symbol": symbol,
+            "average_daily_open_price": total_data["total_daily_open_price"]/total_data["total_count"],
+            "average_daily_close_price": total_data["total_daily_close_price"]/total_data["total_count"],
+            "average_daily_volume": total_data[ "total_daily_volume"]/total_data["total_count"]
+        }
+        return {
+            "data": averaged_obj,
+            "info": {"error" : ''}
+        }
+    except Exception as e:
+        return  {
+            'data': [],
+            'info': {
+                'error': str(e)
+            }
+        }
